e8b8f9f48c1b6594362ffac37807cadc6e5daf3a
hwtinker <hwthinker@gmail.com>
2026-01-11 20:40:04 +0700

    update motor

--- source-code/03-Motor/01-FishBot_Motor_CW_CCW/01-FishBot_Motor_CW_CCW.ino
+++ source-code/03-Motor/01-FishBot_Motor_CW_CCW/01-FishBot_Motor_CW_CCW.ino
@@ -4,15 +4,16 @@
  * Testing: M1(CW)-M1(STOP)-M1(CCW)-M1(Stop)-M2(CW)-M2(STOP)-M2(CCW)-M2(Stop)
  * 
  * GPIO Mapping:
- * Motor 1 (Kiri):  GPIO 12 (Forward/CW), 13 (Backward/CCW)
- * Motor 2 (Kanan): GPIO 22 (Forward/CW), 23 (Backward/CCW)
+ * Motor 2 (Kiri):  GPIO 12 (Forward/CW), 13 (Backward/CCW)
+ * Motor 1 (Kanan): GPIO 22 (Forward/CW), 23 (Backward/CCW)
  */
 
 // Motor Pins
-#define MOTOR1_A 12  // Motor 1 (Kiri) CW/Forward
-#define MOTOR1_B 13  // Motor 1 (Kiri) CCW/Backward
-#define MOTOR2_A 22  // Motor 2 (Kanan) CW/Forward
-#define MOTOR2_B 23  // Motor 2 (Kanan) CCW/Backward
+#define MOTOR1_A 22  // Motor 2 (Kanan) CW/Forward
+#define MOTOR1_B 23  // Motor 2 (Kanan) CCW/Backward
+
+#define MOTOR2_A 12  // Motor 1 (Kiri) CW/Forward
+#define MOTOR2_B 13  // Motor 1 (Kiri) CCW/Backward
 
 void setup() {
   Serial.begin(115200);
@@ -64,33 +65,7 @@ void loop() {
   digitalWrite(MOTOR1_B, LOW);
   delay(3000);
   
-  // 5. M2 CW (Clockwise)
-  Serial.println("5. M2 CW (Clockwise)");
-  digitalWrite(MOTOR1_A, LOW);
-  digitalWrite(MOTOR1_B, LOW);
-  digitalWrite(MOTOR2_A, HIGH);
-  digitalWrite(MOTOR2_B, LOW);
-  delay(3000);
-  
-  // 6. M2 STOP
-  Serial.println("6. M2 STOP");
-  digitalWrite(MOTOR2_A, LOW);
-  digitalWrite(MOTOR2_B, LOW);
-  delay(3000);
-  
-  // 7. M2 CCW (Counter-Clockwise)
-  Serial.println("7. M2 CCW (Counter-Clockwise)");
-  digitalWrite(MOTOR1_A, LOW);
-  digitalWrite(MOTOR1_B, LOW);
-  digitalWrite(MOTOR2_A, LOW);
-  digitalWrite(MOTOR2_B, HIGH);
-  delay(3000);
-  
-  // 8. M2 STOP
-  Serial.println("8. M2 STOP\n");
-  digitalWrite(MOTOR2_A, LOW);
-  digitalWrite(MOTOR2_B, LOW);
-  delay(3000);
+
   
   Serial.println("--- Cycle Complete, Repeat ---\n");
 }
